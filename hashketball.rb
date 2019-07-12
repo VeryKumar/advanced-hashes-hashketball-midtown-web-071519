6e8d75159454da196807fd58c0719f5f95f72a38
Varun <uberkumar@gmail.com>
2019-07-12 21:52:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -98,6 +98,17 @@ def game_hash
           blocks: 5,
           slam dunks: 5
         },
+        {
+          player_name: 'DeSagna Diop',
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam dunks: 5
+        },
         ]
     }
   }
