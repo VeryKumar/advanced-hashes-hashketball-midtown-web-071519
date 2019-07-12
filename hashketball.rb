09d2118cc34307ee95c3142585ef36cfa897b7ad
Varun <uberkumar@gmail.com>
2019-07-12 21:48:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -65,5 +65,17 @@ def game_hash
       team_name: 'Charlotte Hornets'
       colors: ["Turquoise", "White"]
       players: [
+        {
+          player_name: 'Alan Anderson',
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam dunks: 1
+        },
+        ]
     }
   }
